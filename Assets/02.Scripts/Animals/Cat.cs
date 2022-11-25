05f99644232cce3d746c2cb811c6d2ef89a7b11a
SeungB <it.todolist.blake@gmail.com>
2022-11-25 19:07:09 +0900

    cleaning

--- Assets/02.Scripts/Animals/Cat.cs
+++ Assets/02.Scripts/Animals/Cat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elephant : AnimalBase
+public class Cat : AnimalBase
 {
     public void Start()
     {
